848de16c12ae5589e32f0638db15611421c89ce1
Jose Carlos Esteban <jc.esteban.gomez@gmail.com>
2017-07-26 20:40:43 +0200

    Añadido nuevo menu responsive con busquedas con filtro

--- src/js/header.js
+++ src/js/header.js
@@ -1,49 +1,12 @@
-const $ = require("jquery");
-$(function () {
-        // Remove Search if user Resets Form or hits Escape!
-		$('body, .navbar-collapse form[role="search"] button[type="reset"]').on('click keyup', function(event) {
-			console.log(event.currentTarget);
-			if (event.which == 27 && $('.navbar-collapse form[role="search"]').hasClass('active') ||
-				$(event.currentTarget).attr('type') == 'reset') {
-				closeSearch();
-			}
-		});
+var $ = require('jquery');
 
-		//coge el filtro y cambia el select del menu
-		$(document).ready(function(e){
-    		$('.search-panel .dropdown-menu').find('a').click(function(e) {
-				e.preventDefault();
-				var param = $(this).attr("href").replace("#","");
-				var concept = $(this).text();
-				$('.search-panel span#search_concept').text(concept);
-				$('.input-group #search_param').val(param);
-			});
-		});
-
-		function closeSearch() {
-            var $form = $('.navbar-collapse form[role="search"].active')
-    		$form.find('input').val('');
-			$form.removeClass('active');
-			console.log("Formulario busqueda cerrado");
-		}
-
-		// Show Search if form is not active // event.preventDefault() is important, this prevents the form from submitting
-		$(document).on('click', '.navbar-collapse form[role="search"]:not(.active) button[type="submit"]', function(event) {
-			event.preventDefault();
-			var $form = $(this).closest('form'),
-				$input = $form.find('input');
-			$form.addClass('active');
-			$input.focus();
-			console.log("Hola como estas");
-
-		});
-		// ONLY FOR DEMO // Please use $('form').submit(function(event)) to track from submission
-		// if your form is ajax remember to call `closeSearch()` to close the search container
-		$(document).on('click', '.navbar-collapse form[role="search"].active button[type="submit"]', function(event) {
-			event.preventDefault();
-			var $form = $(this).closest('form'),
-				$input = $form.find('input');
-			$('#showSearchTerm').text($input.val());
-            closeSearch()
-		});
+    //Fija el valor del select del buscador de la cabecera.
+    $('.search-panel .dropdown-menu').find('a').click(function(e) {
+		e.preventDefault();
+		var param = $(this).attr("href").replace("#","");
+		var concept = $(this).text();
+		$('.search-panel span#search_concept').text(concept);
+		$('.input-group #search_param').val(param);
     });
+    
+
