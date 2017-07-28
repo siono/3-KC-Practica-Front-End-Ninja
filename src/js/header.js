aef4b42d4ee1df1b55a80725a60aea4b5a21c334
Jose Carlos Esteban <jc.esteban.gomez@gmail.com>
2017-07-28 19:28:55 +0200

    Menu responsive acabado con la refactorización

--- src/js/header.js
+++ src/js/header.js
@@ -1,12 +1,10 @@
 var $ = require('jquery');
 
-    //Fija el valor del select del buscador de la cabecera.
-    $('.search-panel .dropdown-menu').find('a').click(function(e) {
-		e.preventDefault();
-		var param = $(this).attr("href").replace("#","");
-		var concept = $(this).text();
-		$('.search-panel span#search_concept').text(concept);
-		$('.input-group #search_param').val(param);
-    });
-    
-
+//Fija el valor del select del buscador de la cabecera.
+$('.search-panel .dropdown-menu').find('a').click(function (e) {
+	e.preventDefault();
+	var param = $(this).attr("href").replace("#", "");
+	var concept = $(this).text();
+	$('.search-panel span#search_concept').text(concept);
+	$('.input-group #search_param').val(param);
+});
